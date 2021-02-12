ce2d69f259d2e787e2859c2e84d46a6c24920512
dayna coppins <dcoppins.cert@gmail.com>
2021-02-11 20:24:03 -0500

    init

---
+++ force-app/main/default/lwc/setterDemoParent/setterDemoParent.js
@@ -0,0 +1,13 @@
+/**
+ * @description       : 
+ * @author            : dcoppins.cert@gmail.com
+ * @group             : 
+ * @last modified on  : 02-11-2021
+ * @last modified by  : dcoppins.cert@gmail.com
+ * Modifications Log 
+ * Ver   Date         Author                    Modification
+ * 1.0   02-11-2021   dcoppins.cert@gmail.com   Initial Version
+**/
+import { LightningElement } from "lwc";
+
+export default class SetterDemoParent extends LightningElement {}
